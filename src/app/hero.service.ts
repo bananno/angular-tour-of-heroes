e35d69b1a0ad302210b59cc7e802e7bc7b496d59
Anna Peterson <peterson.anna.c@gmail.com>
2019-08-01 16:12:59 -0700

    Display message after fetching hero data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  // This is a typical "service-in-service" scenario: you inject the
+  // MessageService into the HeroService which is injected into the HeroesComponent.
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
